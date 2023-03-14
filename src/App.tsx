c3a9553efd9455ff459391546a1f7b234da2ef5b
BEDEKOVICS Laszlo - IBIS London Earls Court GL3 <H5623-GL3@accor.com>
2023-03-14 14:31:52 +0000

    fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" >
-          Aside
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main" >
