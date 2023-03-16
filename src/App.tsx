d7366e269ab842606d361bdf421491f6ebc1d5b9
BEDEKOVICS Laszlo - IBIS London Earls Court GL3 <H5623-GL3@accor.com>
2023-03-16 17:46:42 +0000

    sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { Genre } from "./hooks/useGenres";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 const App = () => {
@@ -37,7 +38,7 @@ const [gameQuery, setGameQuery] =  useState<GameQuery>({} as GameQuery)
       <GridItem area="main" >
         <HStack spacing={5} paddingLeft={2} marginBottom={5}>
           <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=> setGameQuery({... gameQuery, platform})}/>
-          <SortSelector/>
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({... gameQuery, sortOrder})}/>
         </HStack>
         <GameGrid gameQuery={gameQuery}/>
       </GridItem>
