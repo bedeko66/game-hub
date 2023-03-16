10d24dc61aee0fa612290b24ca2770cc8065f916
BEDEKOVICS Laszlo - IBIS London Earls Court GL3 <H5623-GL3@accor.com>
2023-03-16 13:23:27 +0000

    filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 import { Genre } from "./hooks/useGenres";
 
 const App = () => {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,
@@ -28,8 +29,8 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem area="main" >
-        <PlatformSelector/>
-        <GameGrid selectedGenre={selectedGenre}/>
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform)=> setSelectedPlatform(platform)}/>
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
   );
